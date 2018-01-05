00fbad0bf567037a7e6f860f746a9d58e22742c6
Teddy Widom <theodore.widom@gmail.com>
2018-01-05 10:09:13 -0500

    Specify dependency versions in package.json file (#2)
    * Specify lib dependency versions with package.json
    
    * Update package-lock.json
    
    * Update package-lock.json
    
    * Add newline to end of file
    
    * Add comments to setup.sh
    
    * Tweak
    
    * Tweak

--- setup.sh
+++ setup.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt-get install -y build-essential git curl
+# Install some linux system dependencies
+sudo apt update
+sudo apt install -y build-essential git curl
 
 # Install Node Version Manager
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 
 source ~/.bashrc
 
+# Install version v8.2.1 of the `$ node` command
 nvm install v8.2.1
 
-npm install socket.io node-static pi-blaster.js
+# Install the dependencies required by our application
+# These dependencies are listed in the `package.json` file
+npm install
+
+# Automatically run the node-server.sh shell script when the pi is booted.
+# node-server.sh simply calls `node /path/to/app.js` just as you otherwise would manually.
+sudo cp node-server.sh /etc/init.d/
+sudo update-rc.d node-server.sh defaults
